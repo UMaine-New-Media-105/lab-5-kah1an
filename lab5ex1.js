2c3f4eb1411b722e4a8919151009daa08c05e23f
kah1an <124199658+kah1an@users.noreply.github.com>
2023-03-01 14:07:04 -0500

    forgot to tidy code, so recopy/pasted the code

--- lab5ex1.js
+++ lab5ex1.js
@@ -7,33 +7,33 @@ function setup() {
   createCanvas(400, 400);
   colorMode(HSL);
   angleMode(DEGREES);
-  x = 0
-  y = 0
+  x = 0;
+  y = 0;
   speedX = random(1, 5);
   speedY = random(1, 5);
 }
 
 function draw() {
   background(220);
-  x = x + speedX
-  y = y + speedY
+  x = x + speedX;
+  y = y + speedY;
   addDachshund(x, y, 0.5);
-  if (x >= 360 || x<= -40){
-    speedX = speedX * -1
+  if (x >= 360 || x <= -40) {
+    speedX = speedX * -1;
   }
-  if (y>= 370 || y<= -40){
-    speedY = speedY * -1
+  if (y >= 370 || y <= -50) {
+    speedY = speedY * -1;
   }
 }
 
-function addDachshund(translateX, translateY, size){
+function addDachshund(translateX, translateY, size) {
   push();
   translate(translateX, translateY);
   scale(size);
   noStroke();
   fill(42, 80, 30); //main color
   ellipse(127, 86, 4, 20); //right legs
-  ellipse(80, 86, 4, 20); 
+  ellipse(80, 86, 4, 20);
   ellipse(125, 93.5, 8, 4); //feet
   ellipse(78, 93.5, 8, 4);
   stroke(42, 80, 30); //main color
@@ -47,7 +47,7 @@ function addDachshund(translateX, translateY, size){
   ellipse(34, 50, 20, 30); //neck
   shearX(-20);
   ellipse(45, 42, 35, 30); //head
-  shearX(-30)
+  shearX(-30);
   ellipse(65, 52, 30, 14); //snout
   shearX(30);
   noStroke();
@@ -55,11 +55,11 @@ function addDachshund(translateX, translateY, size){
   ellipse(67, 86, 4, 20);
   ellipse(112, 93.5, 8, 4); //feet
   ellipse(65, 93.5, 8, 4);
-  stroke('black');
+  stroke("black");
   fill(42, 80, 30);
   strokeWeight(1);
   arc(54, 44, 18, 40, -53, 254, OPEN); //ear
-  fill('black');
+  fill("black");
   ellipse(34, 40, 4); //eye
   arc(22, 53, 7, 6, 120, 280); //nose
   noFill();
